38779009bf429c0c94a111b3cae237ebbdacd060
simcard0000 <simran.thind@outlook.com>
2024-01-20 22:26:00 -0500

    remove extraneous commas

--- js/sites.js
+++ js/sites.js
@@ -2,222 +2,222 @@ const allSites = [
     {
         "name": "Simran Thind",
         "year": 2025,
-        "website": "https://www.simranthind.me/",
+        "website": "https://www.simranthind.me/"
     },
     {
         "name": "Janakitti Ratana-Rueangsri",
         "year": 2024,
-        "website": "https://janakitti.com/",
+        "website": "https://janakitti.com/"
     },
     {
         "name": "Anthony Wang",
         "year": 2024,
-        "website": "https://anthonywang.dev/",
+        "website": "https://anthonywang.dev/"
     },
     {
         "name": "Will Harris",
         "year": 2024,
-        "website": "https://willharris.ca/",
+        "website": "https://willharris.ca/"
     },
     {
         "name": "William Qin",
         "year": 2024,
-        "website": "https://williamqin.com/",
+        "website": "https://williamqin.com/"
     },
     {
         "name": "Kelvin Zhang",
         "year": 2024,
-        "website": "https://kelvinzhang.ca/",
+        "website": "https://kelvinzhang.ca/"
     },
     {
         "name": "Zuomiao Hu",
         "year": 2024,
-        "website": "https://zonahu.tech/",
+        "website": "https://zonahu.tech/"
     },
     {
         "name": "Yash Arora",
         "year": 2024,
-        "website": "https://yasharora.com/",
+        "website": "https://yasharora.com/"
     },
     {
         "name": "Jessica Lui",
         "year": 2024,
-        "website": "https://jessicalui.me/",
+        "website": "https://jessicalui.me/"
     },
     {
         "name": "Damian Reiter",
         "year": 2023,
-        "website": "https://damian.bio/",
+        "website": "https://damian.bio/"
     },
     {
         "name": "Jonathan Xu",
         "year": 2025,
-        "website": "https://jonathanxu.com/",
+        "website": "https://jonathanxu.com/"
     },
     {
         "name": "Hannah Guo",
         "year": 2025,
-        "website": "https://hannahguo.me/",
+        "website": "https://hannahguo.me/"
     },
     {
         "name": "Alan Ma",
         "year": 2023,
-        "website": "https://alan-ma.ca/",
+        "website": "https://alan-ma.ca/"
     },
     {
         "name": "Mufeez Amjad",
         "year": 2023,
-        "website": "https://mufeez.me",
+        "website": "https://mufeez.me"
     },
     {
         "name": "Yifei Zhang",
         "year": 2023,
-        "website": "https://yifeiz.com",
+        "website": "https://yifeiz.com"
     },
     {
         "name": "Elena Pan",
         "year": 2025,
-        "website": "https://elena-pan.github.io/",
+        "website": "https://elena-pan.github.io/"
     },
     {
         "name": "Ryan Deng",
         "year": 2024,
-        "website": "https://www.ryandeng.me/",
+        "website": "https://www.ryandeng.me/"
     },
     {
         "name": "Elliott Song",
         "year": 2024,
-        "website": "https://elliottsong.com",
+        "website": "https://elliottsong.com"
     },
     {
         "name": "Bilal Khan",
         "year": 2025,
-        "website": "https://bilal2vec.com",
+        "website": "https://bilal2vec.com"
     },
     {
         "name": "Dylan Snelgrove",
         "year": 2024,
-        "website": "https://dylansnelgrove.com",
+        "website": "https://dylansnelgrove.com"
     },
     {
         "name": "Oren Leung",
         "year": 2025,
-        "website": "https://orenleung.com",
+        "website": "https://orenleung.com"
     },
     {
         "name": "Jason Zhou",
         "year": 2025,
-        "website": "https://zhoujas.github.io/personal/",
+        "website": "https://zhoujas.github.io/personal/"
     },
     {
         "name": "Laura Florea",
         "year": 2024,
-        "website": "https://www.florea.ca",
+        "website": "https://www.florea.ca"
     },
     {
         "name": "Jack Douglas",
         "year": 2024,
-        "website": "https://jackdouglas.tech",
+        "website": "https://jackdouglas.tech"
     },
     {
         "name": "Evan Zhang",
         "year": 2025,
-        "website": "https://evanzhang.ca",
+        "website": "https://evanzhang.ca"
     },
     {
         "name": "Justin Lin",
         "year": 2027,
-        "website": "https://justin-lin.com",
+        "website": "https://justin-lin.com"
     },
     {
         "name": "Donghwui Kim",
         "year": 2027,
-        "website": "https://donghwui.com",
+        "website": "https://donghwui.com"
     },
     {
         "name": "Rajan Agarwal",
         "year": 2028,
-        "website": "https://rajan.sh",
+        "website": "https://rajan.sh"
     },
     {
         "name": "Anthony Chen",
         "year": 2028,
-        "website": "https://chenanthony.com",
+        "website": "https://chenanthony.com"
     },
     {
         "name": "Krish Shah",
         "year": 2026,
-        "website": "https://krishkrosh.com",
+        "website": "https://krishkrosh.com"
     },
     {
         "name": "Jasmine Tai",
         "year": 2025,
-        "website": "https://www.jtai.ca",
+        "website": "https://www.jtai.ca"
     },
     {
         "name": "Alex Zhu",
         "year": 2027,
-        "website": "https://probablyalexzhu.github.io",
+        "website": "https://probablyalexzhu.github.io"
     },
     {
         "name": "Ryan Nguyen",
         "year": 2028,
-        "website": "https://rynguyen.com/",
+        "website": "https://rynguyen.com/"
     },
     {
         "name": "Patrick Gu",
         "year": 2028,
-        "website": "https://patrickgu.ca/",
+        "website": "https://patrickgu.ca/"
     },
     {
         "name": "Ian Korovinsky",
         "year": 2028,
-        "website": "https://iankorovinsky.github.io/",
+        "website": "https://iankorovinsky.github.io/"
     },
     {
         "name": "Rajat Patwari",
         "year": 2025,
-        "website": "https://rajatpatwari.com",
+        "website": "https://rajatpatwari.com"
     },
     {
         "name": "Jomills Jose Anand",
         "year": 2026,
-        "website": "https://jomills.notion.site",
+        "website": "https://jomills.notion.site"
     },
     {
         "name": "Pearl Natalia",
         "year": 2028,
-        "website": "https://pearlnatalia.com",
+        "website": "https://pearlnatalia.com"
     },
     {
         "name": "Michael Pu",
         "year": 2024,
-        "website": "https://mchlp.github.io/",
+        "website": "https://mchlp.github.io/"
     },
     {
         "name": "Krish Modi",
         "year": 2028,
-        "website": "https://krishmodi.com",
+        "website": "https://krishmodi.com"
     },
     {
         "name": "Ishaan Dey",
         "year": 2028,
-        "website": "https://www.ishaand.com/",
+        "website": "https://www.ishaand.com/"
     },
     {
         "name": "Anantjyot Grang",
         "year": 2028,
-        "website": "https://www.anantjyotgrang.com",
+        "website": "https://www.anantjyotgrang.com"
     },
     {
         "name": "Chris Fang",
         "year": 2025,
-        "website": "https://www.chrisfang.dev/",
+        "website": "https://www.chrisfang.dev/"
     },
     {
         "name": "Akshar Barot",
         "year": 2028,
-        "website": "https://www.aksharbarot.com/",
+        "website": "https://www.aksharbarot.com/"
     },
 ];
 
